90f6ef095e7fe70c58f673fe3f8119378e194052
kjhark24 <kjhark24@gmail.com>
2016-09-22 22:11:04 -0400

    Initial prototype
    welp theres the prototype

---
+++ CalvinDiningHall/app/src/androidTest/java/calvin/a262teamf/calvindininghall/ApplicationTest.java
@@ -0,0 +1,13 @@
+package calvin.a262teamf.calvindininghall;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ CalvinDiningHall/app/src/main/java/calvin/a262teamf/calvindininghall/MainActivity.java
@@ -0,0 +1,15 @@
+package calvin.a262teamf.calvindininghall;
+
+import android.app.ActionBar;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
